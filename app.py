3afa00e77da42f91b4cfc23e3e3896cf537d5c2a
wyliadrian <weiyi.li@yale.edu>
2022-12-05 18:37:48 -0500

    Update

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ import bqclient
 # Backend
 def recommendStrategy(keyword):
     # based on the query(a list of names of music), generate a list of music, and return it
-    client = bqclient.create_bqclient("key-file.json")
-    res = bqclient.run_query(client, keyword)["track_name"].values
+    client = bqclient.create_bqclient("setup/key-file.json")
+    res = bqclient.run_query(client, keyword)
     print(res)
     # todo: implement recommend strategy here
     # connect db, and query from database, talk with guys responsible for part 1
-    return res  # a list of music including/related to the keyword
+    return res["track_name"].values  # a list of music including/related to the keyword
 
 
 # Frontend
