c09864452b66b4ffb341bb9bd6e3c552f2a13d62
Ji Yoon Lee <jiyoonlee96@gmail.com>
2022-12-05 18:19:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request
+import bqclient
 
 
 # Backend
 def recommendStrategy(keyword):
     # based on the query(a list of names of music), generate a list of music, and return it
-    res = [keyword] * 10
+    client = bqclient.create_bqclient("key-file.json")
+    res = bqclient.run_query(client, keyword)["track_name"].values
+    print(res)
     # todo: implement recommend strategy here
     # connect db, and query from database, talk with guys responsible for part 1
     return res  # a list of music including/related to the keyword
